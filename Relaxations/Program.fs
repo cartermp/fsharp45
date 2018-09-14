0631d1675fe14f1b8854e5281c556087b9a9b723
cartermp <pcarter@fastmail.com>
2018-09-13 21:47:38 -0700

    Add relaxations

---
+++ Relaxations/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// Collections and subsumption relaxations
+let x0: obj list = [ "a" ]
+let x1: obj list = [ "a"; "b" ]
+let x2: obj list = [ yield "a" :> obj ]
+
+// Indentation relaxation
+let f() =
+    System.Console.WriteLine(format="{0}", arg=[| 
+        "hello"
+        |])
+    System.Console.WriteLine([|
+        "hello"
+        |])
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
